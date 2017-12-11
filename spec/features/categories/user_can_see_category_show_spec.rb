c7c1d1c4f673534ecd85220d3a65dd6f9e87f158
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-11 15:43:20 -0700

    Add edit and delete functionality to category show

--- spec/features/categories/user_can_see_category_show_spec.rb
+++ spec/features/categories/user_can_see_category_show_spec.rb
@@ -1,1 +1,29 @@
 require 'rails_helper'
+
+describe 'As a user' do
+  describe "I see a category show page" do
+    it "displays category title with an option to edit the category" do
+      @entertainment = Category.create(title: "Entertainment")
+
+      visit category_path(@entertainment)
+
+      expect(page).to have_content("Entertainment")
+
+      click_link("Edit")
+
+      expect(current_path).to eq(edit_category_path(@entertainment))
+    end
+
+    it "also has an option to delete the category and redirects to category index" do
+      @entertainment = Category.create(title: "Entertainment")
+
+      visit category_path(@entertainment)
+
+      expect(page).to have_content("Entertainment")
+
+      click_link("Delete")
+
+      expect(current_path).to eq(categories_path)
+    end
+  end
+end
