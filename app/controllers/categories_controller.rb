8a8e2924d7e8a6956ed7f3103beb11ef6708740d
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-11 15:05:37 -0700

    Fix minor spelling error

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,7 +24,7 @@ class CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(title:)
+    params.require(:category).permit(:title)
   end
 
 end
