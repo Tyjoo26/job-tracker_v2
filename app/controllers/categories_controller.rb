02576441cf86d974dee71dd461664b1222ca514e
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-11 15:03:14 -0700

    Modify categories controller for new and create

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,12 +5,26 @@ class CategoriesController < ApplicationController
   end
 
   def new
-
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "You've successfully created a new category"
+
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
 
 
+
+  private
+
+  def category_params
+    params.require(:category).permit(title:)
   end
 
 end
