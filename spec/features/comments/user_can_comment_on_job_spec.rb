f70b5e3f035a681889b95b6d4432a77efc48207a
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-11 18:00:31 -0700

    Migrate Comment table, add references to job table, migrated and updated schema

---
+++ spec/features/comments/user_can_comment_on_job_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe "As a user" do
+  describe "when i go to jobs show page" do
+    before :each do
+      @category = Category.create(title: "Entertainment")
+      @company = Company.create(name: "ESPN")
+      @job = @company.jobs.create(title: "Financal Analyst", description: "Penny Pusher", level_of_interest: 90, city: "Denver", category: @category)
+
+      visit company_job_path(@company, @job)
+    end
+
+    it "displays a form where I can submit a comment and it will redirect me to that show page with displayed content" do
+      expect(page).to have_content("ESPN")
+
+      fill_in"comment[content]", with: "THIS IS A GREAT JOB WOO HOOO!!!"
+
+      click_link("Submit")
+
+      expect(current_path).to eq(company_job_path(@company, @job))
+    end
+  end
+end
