a03a7fa3d4cc7b55433f7fa11b84778147c21370
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-13 21:02:16 -0700

    remigrated, seeded with early dummy data

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,11 @@
 class Job < ApplicationRecord
-  validates :title, :level_of_interest, :city, :category, presence: true
+  validates :title, :level_of_interest, :city, presence: true
   belongs_to :company
   belongs_to :category
   has_many :comments, dependent: :destroy
+
+
+  def self.count_by_level_of_interest
+    count(:level_of_interest)
+  end
 end
